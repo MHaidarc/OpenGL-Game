2c038a286a3770bcc1734fa1f905185e0af1d0d3
Mia Haidar <mateus.haidar@icloud.com>
2024-03-09 21:20:16 -0300

    configured glad correctly

--- src/main.c
+++ src/main.c
@@ -1,31 +1,55 @@
-#include <GL/gl.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <glad/gl.h>
+
 #include <GLFW/glfw3.h>
 
-int main(void) {
-  GLFWwindow *window;
+const GLuint WIDTH = 640, HEIGHT = 480;
 
+static void key_callback(GLFWwindow *window, int key, int scancode, int action,
+                         int mods) {
+  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+
+void error_callback(int error, const char *description) {
+  fprintf(stderr, "Error: %s\n", description);
+}
+
+int main(void) {
   if (!glfwInit())
     return -1;
 
-  window = glfwCreateWindow(1920, 1080, "Window", NULL, NULL);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  GLFWwindow *window =
+      glfwCreateWindow(WIDTH, HEIGHT, "GLFW Window", NULL, NULL);
+
+  glfwMakeContextCurrent(window);
+
+  glfwSetKeyCallback(window, key_callback);
+  glfwSetErrorCallback(error_callback);
 
   if (!window) {
     glfwTerminate();
     return -1;
   }
 
-  glfwMakeContextCurrent(window);
+  int version = gladLoadGL(glfwGetProcAddress);
+  printf("OpenGL Version %d.%d\n", GLAD_VERSION_MAJOR(version),
+         GLAD_VERSION_MINOR(version));
 
   while (!glfwWindowShouldClose(window)) {
+    glfwPollEvents();
 
     glClear(GL_COLOR_BUFFER_BIT);
 
     glfwSwapBuffers(window);
-
-    glfwPollEvents();
   }
 
   glfwTerminate();
-
   return 0;
 }
