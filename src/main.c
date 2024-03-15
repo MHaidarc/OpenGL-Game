b2b6b801ae55d7f19d9fac0c43eea6848d338185
haidaar <mateus.haidar@icloud.com>
2024-03-14 22:01:31 -0300

    now following learnopengl.com guide for rendering, will begin development shortly after

--- src/main.c
+++ src/main.c
@@ -1,136 +1,70 @@
-
-#define GLAD_GL_IMPLEMENTATION
 #include <glad/gl.h>
-#define GLFW_INCLUDE_NONE
+
 #include <GLFW/glfw3.h>
-
-#include "linmath.h"
-
-#include <stddef.h>
 #include <stdio.h>
-#include <stdlib.h>
 
-typedef struct Vertex {
-  vec2 pos;
-  vec3 col;
-} Vertex;
+// function prototypes
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+void process_input(GLFWwindow *window);
 
-static const Vertex vertices[3] = {{{-0.6f, -0.4f}, {1.f, 0.f, 0.f}},
-                                   {{0.6f, -0.4f}, {0.f, 1.f, 0.f}},
-                                   {{0.f, 0.6f}, {0.f, 0.f, 1.f}}};
-
-static const char *vertex_shader_text =
-    "#version 330\n"
-    "uniform mat4 MVP;\n"
-    "in vec3 vCol;\n"
-    "in vec2 vPos;\n"
-    "out vec3 color;\n"
-    "void main()\n"
-    "{\n"
-    "    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
-    "    color = vCol;\n"
-    "}\n";
-
-static const char *fragment_shader_text = "#version 330\n"
-                                          "in vec3 color;\n"
-                                          "out vec4 fragment;\n"
-                                          "void main()\n"
-                                          "{\n"
-                                          "    fragment = vec4(color, 1.0);\n"
-                                          "}\n";
-
-static void error_callback(int error, const char *description) {
-  fprintf(stderr, "Error: %s\n", description);
-}
-
-static void key_callback(GLFWwindow *window, int key, int scancode, int action,
-                         int mods) {
-  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-    glfwSetWindowShouldClose(window, GLFW_TRUE);
-}
-
-int main(void) {
-  glfwSetErrorCallback(error_callback);
-
-  if (!glfwInit())
-    exit(EXIT_FAILURE);
-
+int main() {
+  // initializing GLFW and setting the OpenGL version
+  glfwInit();
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-  GLFWwindow *window =
-      glfwCreateWindow(640, 480, "OpenGL Triangle", NULL, NULL);
-  if (!window) {
+  // glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); //required for MacOS
+  // compilation
+
+  // creating the GLFW window, if it doesn't open, throw an error and terminate
+  // GLFW
+  GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+
+  if (window == NULL) {
+    fprintf(stderr, "Failed to create GLFW window\n");
     glfwTerminate();
-    exit(EXIT_FAILURE);
+    return -1;
   }
 
-  glfwSetKeyCallback(window, key_callback);
-
   glfwMakeContextCurrent(window);
-  gladLoadGL(glfwGetProcAddress);
-  glfwSwapInterval(1);
 
-  // NOTE: OpenGL error checks have been omitted for brevity
+  // load GLAD2 extension loader
+  if (!gladLoadGL(glfwGetProcAddress)) {
+    fprintf(stderr, "Failed to initialize GLAD\n");
+    return -1;
+  }
 
-  GLuint vertex_buffer;
-  glGenBuffers(1, &vertex_buffer);
-  glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-  const GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-  glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
-  glCompileShader(vertex_shader);
-
-  const GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-  glShaderSource(fragment_shader, 1, &fragment_shader_text, NULL);
-  glCompileShader(fragment_shader);
-
-  const GLuint program = glCreateProgram();
-  glAttachShader(program, vertex_shader);
-  glAttachShader(program, fragment_shader);
-  glLinkProgram(program);
-
-  const GLint mvp_location = glGetUniformLocation(program, "MVP");
-  const GLint vpos_location = glGetAttribLocation(program, "vPos");
-  const GLint vcol_location = glGetAttribLocation(program, "vCol");
-
-  GLuint vertex_array;
-  glGenVertexArrays(1, &vertex_array);
-  glBindVertexArray(vertex_array);
-  glEnableVertexAttribArray(vpos_location);
-  glVertexAttribPointer(vpos_location, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex),
-                        (void *)offsetof(Vertex, pos));
-  glEnableVertexAttribArray(vcol_location);
-  glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex),
-                        (void *)offsetof(Vertex, col));
-
+  // rendering loop
   while (!glfwWindowShouldClose(window)) {
-    int width, height;
-    glfwGetFramebufferSize(window, &width, &height);
-    const float ratio = width / (float)height;
+    // input
+    process_input(window);
 
-    glViewport(0, 0, width, height);
+    /*
+     * rendering
+     * TODO: rendering
+     */
+
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
-    mat4x4 m, p, mvp;
-    mat4x4_identity(m);
-    mat4x4_rotate_Z(m, m, (float)glfwGetTime());
-    mat4x4_ortho(p, -ratio, ratio, -1.f, 1.f, 1.f, -1.f);
-    mat4x4_mul(mvp, p, m);
-
-    glUseProgram(program);
-    glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat *)&mvp);
-    glBindVertexArray(vertex_array);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
-
+    // check and call events and swap the buffers
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
 
-  glfwDestroyWindow(window);
-
   glfwTerminate();
-  exit(EXIT_SUCCESS);
+  printf("GLFW terminated successfully\n");
+  return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+  glViewport(0, 0, width, height);
+}
+
+void process_input(GLFWwindow *window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, GL_TRUE);
 }
